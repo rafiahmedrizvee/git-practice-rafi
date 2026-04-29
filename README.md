c8be723791eea5ce4d5332805d3f63007e3f8021
rafiahmedrizvee <rizvirafi7@gmail.com>
2026-04-29 21:59:52 +0600

    updated

---
+++ README.md
@@ -0,0 +1,6 @@
+# Git Practice Project
+
+Name: Rafi
+
+## Description
+This project demonstrates basic Git and GitHub operations including commits, branching, and merging.
---
+++ main.py
@@ -0,0 +1,8 @@
+from datetime import datetime
+from utils import add, subtract
+
+print("Name: Rafi")
+print("Today's Date:", datetime.today().date())
+
+print("Add:", add(5, 3))
+print("Subtract:", subtract(5, 3))
